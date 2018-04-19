09e9dba965e5d33625fffb5c1edba5dcb029475a
alfaroleonsebas97 <37912510+alfaroleonsebas97@users.noreply.github.com>
2018-04-19 12:25:04 -0600

    clase ListaOrdenada completa y pasa las pruebas

--- ListaOrdenada.cpp
+++ ListaOrdenada.cpp
@@ -7,18 +7,18 @@
 
 #include "ListaOrdenada.h"
 
-ListaOrdenada::ListaOrdenada(): inicio(nullptr) {
+ListaOrdenada::ListaOrdenada() : inicio(nullptr) {
 }
 
-ListaOrdenada::ListaOrdenada(const ListaOrdenada& orig){
+ListaOrdenada::ListaOrdenada(const ListaOrdenada& orig) {
     shared_ptr<Nodo> p = orig.inicio;
-    if (p == nullptr){
+    if (p == nullptr) {
         inicio = nullptr;
-    }else{
+    } else {
         inicio = shared_ptr<Nodo>(new Nodo(orig.inicio->pos));
         p = p->sgt;
         shared_ptr<Nodo> ultimo = inicio;
-        while(p != nullptr){
+        while (p != nullptr) {
             ultimo->sgt = shared_ptr<Nodo>(new Nodo(p->pos));
             p = p->sgt;
             ultimo = ultimo->sgt;
@@ -33,40 +33,40 @@ void ListaOrdenada::agregar(int nPos) {
     bool rsl = false;
     shared_ptr<Nodo> p = inicio;
     shared_ptr<Nodo> ant = nullptr;
-    if(inicio == nullptr){
+    if (inicio == nullptr) {
         inicio = shared_ptr<Nodo>(new Nodo(nPos));
-	cntAdy++;
+        cntAdy++;
         rsl = true;
-    } else{
-        if (inicio->pos > nPos){ 
-        p = shared_ptr<Nodo>(new Nodo(nPos)); 
-	cntAdy++;
-        p->sgt = inicio; 
-        inicio = p; 
-        rsl = true;
-        } else{
+    } else {
+        if (inicio->pos > nPos) {
+            p = shared_ptr<Nodo>(new Nodo(nPos));
+            cntAdy++;
+            p->sgt = inicio;
+            inicio = p;
+            rsl = true;
+        } else {
             p = inicio;
-            while(p != nullptr){
-                if(p->pos == nPos){ 
+            while (p != nullptr) {
+                if (p->pos == nPos) {
                     p = nullptr;
-                } else{ 
-                    if(p->pos < nPos){
+                } else {
+                    if (p->pos < nPos) {
                         ant = p;
                         p = p->sgt;
-                        if (p == nullptr){
+                        if (p == nullptr) {
                             rsl = true;
                         }
-                    } else { 
+                    } else {
                         p = nullptr;
-                        rsl = true; 
+                        rsl = true;
                     }
                 }
             }
-            if (rsl){
+            if (rsl) {
                 p = ant->sgt;
                 ant->sgt = shared_ptr<Nodo>(new Nodo(nPos));
-		cntAdy++;
-                ant->sgt->sgt  = p;
+                cntAdy++;
+                ant->sgt->sgt = p;
             }
         }
     }
@@ -75,15 +75,14 @@ void ListaOrdenada::agregar(int nPos) {
 bool ListaOrdenada::buscar(int nPos) {
     bool rsl = false;
     shared_ptr<Nodo> p = inicio;
-    while(p != nullptr){
-        if(p->pos == nPos){
+    while (p != nullptr) {
+        if (p->pos == nPos) {
             rsl = true;
             p = nullptr;
-        }else{
-            if(p->pos < nPos){
+        } else {
+            if (p->pos < nPos) {
                 p = p->sgt;
-            }
-            else{
+            } else {
                 p = nullptr;
             }
         }
@@ -92,28 +91,34 @@ bool ListaOrdenada::buscar(int nPos) {
 }
 
 string ListaOrdenada::toString() {
- stringstream fs;
+    stringstream fs;
     fs << '{';
     shared_ptr<Nodo> p = inicio;
-    while (p != nullptr){
+    while (p != nullptr) {
         fs << p->pos;
-        if(p->sgt != nullptr){
+        if (p->sgt != nullptr) {
             fs << ',';
-            p = p->sgt; 
-        }  
+            p = p->sgt;
+        } else {
+            p = nullptr;
+        }
     }
-        fs << '}';
-        return fs.str();
+    fs << '}';
+    return fs.str();
 }
 
 int* ListaOrdenada::adyacencias() {
     int* arreglo = new int[cntAdy];
     shared_ptr<Nodo> p = inicio;
-    int k = 0;
-    while(p!=nullptr){
-	arreglo[k] = p->pos;
-	p = p->sgt;
-	k++;
+    if (p == nullptr) {
+        arreglo = nullptr;
+    } else {
+        int k = 0;
+        while (p != nullptr) {
+            arreglo[k] = p->pos;
+            p = p->sgt;
+            k++;
+        }
     }
     return arreglo;
 }
